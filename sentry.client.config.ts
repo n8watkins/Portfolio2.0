6aae054dc738424110bc381ebf69804bcd7f42f2
Nathan Watkins <nathancwatkins23@gmail.com>
2024-08-21 16:01:16 -0700

    update sentry

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -1,6 +1,7 @@
 // This file configures the initialization of Sentry on the client.
 // The config you add here will be used whenever a users loads a page in their browser.
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
 import * as Sentry from '@sentry/nextjs'
 
 Sentry.init({
@@ -26,7 +27,7 @@ Sentry.init({
     }),
     Sentry.feedbackIntegration({
       // Additional SDK configuration goes in here, for example:
-      colorScheme: 'light',
+      colorScheme: 'dark',
     }),
   ],
 })
