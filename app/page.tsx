8ac0ffd83a04ba852f4e559217493ea6f48918ea
Nathan Watkins <nathancwatkins23@gmail.com>
2024-10-09 12:22:46 -0700

    removing breakpoint

--- app/page.tsx
+++ app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
           <Experience />
           <Clients />
           <Footer />
-          <BreakpointDisplay />
+          {/* <BreakpointDisplay /> */}
         </div>
       </div>
     </main>
