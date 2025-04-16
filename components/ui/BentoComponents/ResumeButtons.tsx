cd593877708c3beb3e398508817fc3d44b817ee0
Nathan Watkins <nathancwatkins23@gmail.com>
2025-04-16 12:28:03 -0700

    resume

--- components/ui/BentoComponents/ResumeButtons.tsx
+++ components/ui/BentoComponents/ResumeButtons.tsx
@@ -45,7 +45,7 @@ const ResumeButtons: React.FC<ResumeButtonsProps> = ({ buttonContainer, buttonCl
             <a
               target="_blank"
               href={
-                'https://drive.google.com/file/d/1vG1YmT0yyIraAlUEImABE6Srp0eru9Gh/view?usp=sharing'
+                'https://drive.google.com/file/d/1B_I_aQ3Kb7PTTdYtXD0DX_ubRBHVjuBG/view?usp=sharing'
               }>
               <button
                 className={`${buttonClass} gap-3 bg-[linear-gradient(110deg,#1e40af,45%,#2563eb,55%,#1e40af)] border-white/30 animate-duration-[5000ms]`}>
@@ -54,7 +54,7 @@ const ResumeButtons: React.FC<ResumeButtonsProps> = ({ buttonContainer, buttonCl
               </button>
             </a>
             <a
-              href="https://drive.google.com/uc?export=download&id=1vG1YmT0yyIraAlUEImABE6Srp0eru9Gh"
+              href="https://drive.google.com/uc?export=download&id=1B_I_aQ3Kb7PTTdYtXD0DX_ubRBHVjuBG"
               download="Nathan_Watkins_NextJS_Resume">
               <button
                 className={`${buttonClass} gap-2 bg-[linear-gradient(40deg,#1e40af,45%,#2563eb,55%,#1e40af)] border-white/30 animate-duration-[6000ms]`}>
